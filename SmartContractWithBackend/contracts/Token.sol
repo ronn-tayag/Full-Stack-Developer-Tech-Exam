e7f98689806e8975bc0c9e19bae75bc2c422cba3
Ronnie Tayag <ronnie.tayag.20@gmail.com>
2024-10-21 23:39:39 +0800

    update contract

--- SmartContractWithBackend/contracts/Token.sol
+++ SmartContractWithBackend/contracts/Token.sol
@@ -5,7 +5,9 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract MyToken is ERC20, Ownable {
-    constructor() ERC20("RonnToken", "RON") Ownable(msg.sender){}
+    constructor() ERC20("RonnToken", "RON") Ownable(msg.sender){
+        _mint(msg.sender, 1000 * 10 ** decimals());
+    }
 
     // Mint function allows the owner to create new tokens
     function mint(address to, uint256 amount) public onlyOwner {
